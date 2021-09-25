0941d09d7f205a4233c5345962b2b900f367925b
xuan <13228002836@163.com>
2021-09-25 22:33:39 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+// 引入vue-loader的插件
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  mode: 'development',
+  // 打包入口
+  entry: './src/main.js',
+  // 打包出口
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  // 打包规则
+  module: {
+    rules: [{
+      test: /\.vue$/,
+      loader: 'vue-loader'
+    }]
+  },
+  // 插件
+  plugins: [
+    new VueLoaderPlugin
+  ],
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.js'
+    }
+  }
+}
